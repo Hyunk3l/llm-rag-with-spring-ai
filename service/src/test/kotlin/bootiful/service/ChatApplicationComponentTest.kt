ddc7a91450cd0545f78e324e7ec76960279c54ca
Fabri Di Napoli <hyunk3l@gmail.com>
2024-03-31 19:39:27 +0200

    Add skeleton test

---
+++ service/src/test/kotlin/bootiful/service/ChatApplicationComponentTest.kt
@@ -0,0 +1,11 @@
+package bootiful.service
+
+import org.junit.jupiter.api.Test
+
+class ChatApplicationComponentTest {
+
+    @Test
+    fun `should ask a query to chatgpt using RAG`() {
+
+    }
+}

89a62cdd82944cf642849dbf06fb70d918308546
Fabri Di Napoli <hyunk3l@gmail.com>
2024-04-28 00:36:41 +0200

    Add component test

--- service/src/test/kotlin/bootiful/service/ChatApplicationComponentTest.kt
+++ service/src/test/kotlin/bootiful/service/ChatApplicationComponentTest.kt
@@ -1,32 +1,49 @@
 package bootiful.service
 
-import org.junit.jupiter.api.BeforeAll
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
-import kotlin.test.DefaultAsserter.assertEquals
 
-@SpringBootTest
-class ChatApplicationComponentTest {
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = ["wiremock.port=7777", "spring.ai.openai.base-url=http://localhost:7777"]
+)
+class StaticTest {
 
     @Test
-    fun `should ask a query to chatgpt using RAG`() {
-        val capturedOutput = outputStream.toString()
-        assertEquals("Don't match", """""
-            {response=Under the new employment model, Consumer Direct Care Network Washington (CDWA) has replaced the Department of Social and Health Services (DSHS) as the new administrative employer for Individual Providers (IPs) in Washington. CDWA is now providing employment support for IPs, while consumers will continue as managing employers. This transition allows DSHS to focus on consumer case management. Consumers should work with their case managers to create a Plan of Care and access needed services. Once a consumer has chosen an IP to work with, they should complete the CDWA hiring requirements. It's important not to schedule an IP to work or start work until CDWA has provided an Okay to Work notification. Consumers can refer to the Managing Employer Handbook for more details on CDWA’s Consumer Resources. All IPs need to complete the CDWA hiring requirements, and consumers need to have an Okay to Work from CDWA before care can start. For more information, IPs can refer to the IP Employment Handbook on CDWA’s IP Resources.}
-        """.trimIndent(), capturedOutput)
+    fun `some`() {
+
     }
 
     companion object {
-
-        private lateinit var outputStream: ByteArrayOutputStream
         @JvmStatic
-        @BeforeAll
-        fun setUp(): Unit {
-            outputStream = ByteArrayOutputStream()
-            val printStream = PrintStream(outputStream)
-            System.setOut(printStream)
+        val mockHttpServer: WireMockServer = startWiremock()
+
+        private fun startWiremock(): WireMockServer {
+            val wireMockServer = WireMockServer(7777)
+            wireMockServer.start()
+            WireMock.configureFor("localhost", 7777)
+            val response = this::class.java.getResource("wiremock/response.json")?.getContent().toString()
+            WireMock.stubFor(
+                    WireMock.post(WireMock.urlEqualTo("/v1/embeddings"))
+                            .willReturn(
+                                    WireMock.aResponse()
+                                            .withStatus(200)
+                                            .withHeader("Content-Type", "application/json")
+                                            .withBody(response)
+                            )
+            )
+            WireMock.stubFor(
+                    WireMock.post(WireMock.urlEqualTo("/v1/chat/completions")).willReturn(
+                            WireMock.aResponse()
+                                    .withStatus(200)
+                                    .withBody(response)
+                    )
+            )
+            println("Running from static? ${wireMockServer.isRunning}")
+            return wireMockServer
         }
     }
 }
